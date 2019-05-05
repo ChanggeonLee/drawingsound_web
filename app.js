3c5d155d18002d4e0dc1834684893f5bb2817873
leehayeong <qufgkdud@gmail.com>
2019-05-05 22:51:36 +0900

    add faq page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var homeRouter = require('./routes/home');
 var sheetRouter = require('./routes/sheet');
+var faqRouter = require('./routes/faq');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/home', homeRouter);
 app.use('/sheet', sheetRouter);
+app.use('/faq', faqRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
