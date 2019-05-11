8790e1e8b1181b490b272d6ccb56d49ea248bd97
LeeChangGeon <changgeon718@gmail.com>
2019-05-11 22:03:30 +0900

    add flash

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+var flash = require('connect-flash');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var homeRouter = require('./routes/home');
@@ -22,6 +25,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// session을 사용
+app.use(session({
+  name: 'mjoverflow',
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-longlonglong123asdasdaszxcasdq1123123sdasdlkjlkjaflkvna;ls123'
+}));
+// flash
+app.use(flash()); // flash message를 사용할 수 있도록
+
+// pug의 local에 현재 사용자 정보와 flash 메시지를 전달하자.
+app.use(function(req, res, next) {  
+  // res.locals.currentUser = req.user; //passport는 req.user로 user정보 전달
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+// router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/home', homeRouter);
--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,28 @@ const config = require('./lib/firebase_config');
 
 firebase.initializeApp(config);
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.post('/signin', async(req, res, next) => {
+  req.flash('success', '로그인 성공~!');    
+  // req.flash('success', 'This is a flash message using the express-flash module.');
+
+  // a = await firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {  
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;  
+  // });
+
+  // console.log(a);
+
+  // console.log(req.body);    
+  res.redirect('/home');
+});
+
 router.get('/list', function(req, res, next){
-  // var database = firebase.database();
+  // var database = firebase.database();    
   return firebase.database().ref('/sheets').once('value').then(function(snapshot) {
     var username = (snapshot.val()) || 'Anonymous';
     res.send(username);
