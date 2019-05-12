b74691e958fb95d042e8d80e712a4b4951b2a080
LeeChangGeon <changgeon718@gmail.com>
2019-05-12 17:20:08 +0900

    sheets

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var needauth = require('./lib/need-auth');
+var firebase = require('firebase');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
+router.get('/', needauth, (req, res, next) => {
+  var user = firebase.auth().currentUser;
+  var uid = user.uid;
+
+  return firebase.database().ref('/sheets/'+uid).once('value').then(function(snapshot) {
+    console.log(uid);
+    var sheets = snapshot.val();    
+    console.log(sheets);
+    res.render('home',{Sheets:sheets});  
+  });
 });
 
 
--- routes/sheet.js
+++ routes/sheet.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', );
 });
 
 router.get('/show', function(req, res, next) {
-  res.render('sheet/show', { title: 'Express' });
+  res.render('sheet/show', );
 });
 
 module.exports = router;
