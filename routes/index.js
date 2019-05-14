0ba7b2c92332aea8c9c4248fd6b9da5c5b527a83
LeeChangGeon <changgeon718@gmail.com>
2019-05-15 01:04:40 +0900

    add sheet

--- routes/index.js
+++ routes/index.js
@@ -54,7 +54,6 @@ router.get('/test', function(req, res, next){
   //   str : 'X:1\nT: Cooley\'s\nM: 4/4\nL: 1/8\nR: reel\nK: Emin\nD2|:"Em"EB{c}BA B2 EB|~B2 AB dBAG|"D"FDAD BDAD|FDAD dAFD|\n"Em"EBBA B2 EB|B2 AB defg|"D"afe^c dBAF|1"Em"DEFD E2 D2:|2"Em"DEFD E2 gf||\n|:"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|\n"Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|1"Em"DEFD E2 gf:|2"Em"DEFD E4|]\n'
   // };
   // res.render('test', {Sheet:sheetdata});
-
 });
 
 
