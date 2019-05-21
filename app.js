92c9f9379cffe53b80d9c742e399f4d4de0087b3
LeeChangGeon <changgeon718@gmail.com>
2019-05-21 21:55:13 +0900

    delete key

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var methodOverride = require('method-override');
 var session = require('express-session');
 var flash = require('connect-flash');
 
@@ -19,11 +19,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+
 
 // session을 사용
 app.use(session({
--- routes/sheet.js
+++ routes/sheet.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var needauth = require('./lib/need-auth');
 var firebase = require('firebase');
+var catcherror = require('./lib/async-error');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,19 +12,11 @@ router.get('/', function(req, res, next) {
 router.get('/show', needauth, function(req, res, next) {
   var user = firebase.auth().currentUser;
   var uid = user.uid;
+  var key = req.query.key;
   var url = '/sheets/' + uid + '/' + req.query.key + '/';  
   return firebase.database().ref(url).once('value').then(function(snapshot) {
     var sheet = (snapshot.val());
-    res.render('sheet/show', {Sheet:sheet});
-  });  
-});
-
-router.get('/test', function(req, res, next) {
-  var url = '/sheets/' + req.query.uid + '/' + req.query.key + '/';  
-  console.log(req.query.key);
-  return firebase.database().ref(url).once('value').then(function(snapshot) {
-    var sheet = (snapshot.val());
-    res.render('sheet/show', {Sheet:sheet});
+    res.render('sheet/show', {Sheet:sheet, Key:key});
   });  
 });
 
@@ -36,5 +29,16 @@ router.get('/msheet',function(req,res,next) {
     res.render('sheet/mobile', {Sheet:sheet});
   });
 });
+
+router.delete('/delete/:key/',needauth,catcherror( async(req,res,next) => {
+  var user = firebase.auth().currentUser;
+  uid = user.uid;
+  console.log(req.params.key);
+  // var url = '/sheets/' + uid + '/' + req.body.key + '/'; 
+  // firebase.database().ref(url).remove();
+  firebase.database().ref().child('sheets').child(uid).child(req.params.key).remove();
+  req.flash('danger', "악보 삭제 완료");
+  res.redirect('/home');
+}));
 
 module.exports = router;
