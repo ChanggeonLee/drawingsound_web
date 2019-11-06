f89c5a2b6ccff4ca6d4d1d21c8aab57968d6d60d
changgeonLee <changgeon718@gmail.com>
2019-11-06 21:56:13 +0900

    Bug ifx

--- routes/model.js
+++ routes/model.js
@@ -8,13 +8,10 @@ const exec = require('await-exec');
 /* GET home page. */
 router.get('/:fname', async (req, res, next) => {
     var audio_path = './deepmusic/data/'+req.params.fname;
-    // console.log(audio_path);
     var cmd = 'python ./deepmusic/for_express_deep_music.py ' + audio_path;
     var result; 
 
-    // const {result ,stderr} = await exec(cmd);
     const { stdout, stderr } = await exec(cmd);
-    // console.log(stdout);
     var info = {
         music_name : stdout,
     }
