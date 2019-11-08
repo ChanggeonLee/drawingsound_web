ba448e442c257dfd27d706349df060198b26b3ec
Ubuntu <ubuntu@ip-172-31-42-179.us-east-2.compute.internal>
2019-11-08 17:46:55 +0000

    Bug fix

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var methodOverride = require('method-override');
 var session = require('express-session');
 var flash = require('connect-flash');
 var favicon = require('serve-favicon');
+var bodyParser = require('body-parser')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +22,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
+
+app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
 
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/model.js
+++ routes/model.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var multer  = require('multer')
-var upload = multer({ dest: 'wave/' })
+var multer  = require('multer');
+var fs = require('fs');
 
 const exec = require('await-exec');
-
 
 // /* GET home page. */
 // router.get('/:fname', async (req, res, next) => {
@@ -19,8 +18,17 @@ const exec = require('await-exec');
 //     res.json(info)
 // });
 
-/* GET home page. */
+router.post('/musicname', async (req, res, next) => {
+	fs.writeFile('wave/data.wav', req.body, function(err) {
+	});
+	res.send("test");
+    	res.end();
+
+});
+
+/*
 router.post('/musicname', upload.single('wave'), async (req, res, next) => {
+	console.log(req.file);
     var audio_path =  req.file.path;
     var cmd = 'python ./deepmusic/for_express_deep_music.py ' + audio_path;
 
@@ -29,7 +37,7 @@ router.post('/musicname', upload.single('wave'), async (req, res, next) => {
         music_name : stdout,
     }
     res.json(info);
-});
+});*/
 
 
 
