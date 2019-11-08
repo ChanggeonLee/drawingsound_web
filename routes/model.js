c6b36327f1c5f60ce9b4b16f77e3d934a8f13b1c
changgeonLee <changgeon718@gmail.com>
2019-11-08 20:25:21 +0900

    Add multer

--- routes/model.js
+++ routes/model.js
@@ -1,22 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var needauth = require('./lib/need-auth');
+var multer  = require('multer')
+var upload = multer({ dest: 'wave/' })
 
 const exec = require('await-exec');
 
 
+// /* GET home page. */
+// router.get('/:fname', async (req, res, next) => {
+//     var audio_path = './deepmusic/data/'+req.params.fname;
+//     var cmd = 'python ./deepmusic/for_express_deep_music.py ' + audio_path;
+//     var result; 
+
+//     const { stdout, stderr } = await exec(cmd);
+//     var info = {
+//         music_name : stdout,
+//     }
+//     res.json(info)
+// });
+
 /* GET home page. */
-router.get('/:fname', async (req, res, next) => {
-    var audio_path = './deepmusic/data/'+req.params.fname;
+router.post('/musicname', upload.single('wave'), async (req, res, next) => {
+    var audio_path =  req.file.path;
     var cmd = 'python ./deepmusic/for_express_deep_music.py ' + audio_path;
-    var result; 
 
     const { stdout, stderr } = await exec(cmd);
     var info = {
         music_name : stdout,
     }
-    res.json(info)
+    res.json(info);
 });
+
 
 
 module.exports = router;
